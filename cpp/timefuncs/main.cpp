6540bf383e52e6efa91dd823bfa194a304e5bab2
Siddhanth Mate <siddhanthmate@Siddhanths-MacBook-Pro.local>
2024-08-11 15:50:33 +0530

    added time functions c++

--- cpp/timefuncs/main.cpp
+++ cpp/timefuncs/main.cpp
@@ -1,10 +1,48 @@
 #include "main.h"
 #include <ctime>
+#include <
 
-std::tm OptionsTimeFunctions::SetEod(){
-            this->tm.tm_hour = 15;
-            this->tm.tm_min = 30;
-            this->tm.tm_sec = 0;
-            return this->tm;
+std::tm OptionsTimeFunctions::SetEndOfDay(){
+    std::tm eod_tm;
+    eod_tm.tm_hour = this->hour;
+    eod_tm.tm_min = this->min;
+    eod_tm.tm_sec = this->sec;
+    return eod_tm;
 }
 
+std::vector<OptionData> TimeFilter(const std::vector<OptionData>& OptionDataVec,
+                                    const std::tm& start_trade_time,
+                                    const std::tm& end_trade_time){
+                            
+            std::vector<OptionData> FilteredData;
+            FilteredData.reserve(OptionDataVec.size());
+
+            int start_hour = start_trade_time.tm_hour;
+            int end_hour = end_trade_time.tm_hour;
+
+            int start_min = start_trade_time.tm_min;
+            int end_min = end_trade_time.tm_min;
+
+            int start_sec = start_trade_time.tm_sec;
+            int end_sec = end_trade_time.tm_sec;
+
+            const auto start_time_tuple = std::make_tuple(start_hour,
+                                                          start_min,
+                                                          start_sec);
+
+            const auto end_time_tuple = std::make_tuple(end_hour,
+                                                        end_min,
+                                                        end_sec);
+
+            for(const auto& data: OptionDataVec){
+                const std::tm& curr_time = data.datetime;
+
+                auto current_time_tuple = std::make_tuple(curr_time.tm_hour,
+                                                    curr_time.tm_min,
+                                                    curr_time.tm_sec);
+
+                if((current_time_tuple>= start_time_tuple) && (current_time_tuple<=end_time_tuple)){
+                    FilteredData.push_back(data);
+                }
+            }
+}
--- cpp/timefuncs/main.h
+++ cpp/timefuncs/main.h
@@ -8,17 +8,27 @@
 #include <string>
 #include <ctime>
 
+#include "../InstrumentStruct/OptionStruct.h"
+
 
 class OptionsTimeFunctions {
     private:
-        std::tm tm;
+        int hour;
+        int min;
+        int sec;
     public:
-        OptionsTimeFunctions(const std::tm& tm){
-            this->tm = tm;
+        OptionsTimeFunctions(const int& hour,
+                             const int& min,
+                             const int& sec){
+            this->hour = hour;
+            this->min = min;
+            this->sec = sec;
         }
-        std::tm SetEod();
-        
 
+        std::tm SetEndOfDay();
+        std::vector<OptionData> TimeFilter(const std::vector<OptionData>& OptionDataVec,
+                                           const std::tm& start_trade_time,
+                                           const std::tm& end_trade_time);
 };
 
 
